c608c9b21434216061db964ae3b300c42e68bb23
ErikRoman <118341316+ErikRoman@users.noreply.github.com>
2022-12-27 19:08:58 +0700

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // Set up index.html to load the bundle
 // Make sure to load uuid via an npm module when necessary
 
+import { setFilters } from "./filters";
+import { renderTodos } from "./views";
+
 // --
 
 // Add necessary imports
@@ -16,3 +19,27 @@
 // Bonus: Add a watcher for local storage
 
 
+renderTodos()
+
+
+// Set up search text handler
+document.querySelector( '#search-text' ).addEventListener( 'input', ( event ) {
+  setFilters( {
+    searchText: event.target.value
+  } )
+
+  renderTodos()
+} )
+
+
+// Set up checkbox handler
+document.querySelector( '#hide-completed' ).addEventListener( 'change', ( event ) {
+  setFilters( {
+    hideCompleted: event.target.checked
+  } )
+
+  renderTodos()
+} )
+
+
+// Set up form submission handler
